52fe04792b8d053f5db4e349284469f347969e83
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-04-09 14:30:29 +0100

    Add another monster, so we call dnmonster on each request

--- app/identidock.py
+++ app/identidock.py
@@ -22,20 +22,27 @@ def hit_me(name):
     counter = 'hits-{0}'.format(name)
     cache.incr(counter)
 
-    return 'I have been seen %i times.' % int(cache.get(counter))
+    return int(cache.get(counter))
 
 @app.route('/')
 def mainpage():
 
+    hits = hit_me(name)
     salted_name = salt + name
     name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
+    friend_name = '{0} [#{1}]'.format(request.headers.get('User-Agent'), hits)
+    friend_hash = hashlib.sha256(friend_name.encode()).hexdigest()
     header = '<html><head><title>IdentiOrca</title></head><body>'
     body = '''<h2>Hello! My name is {0}.</h2>
               <p/>
-              <strong><em>{1}</em></strong>
+              <strong><em>I have been seen {1} times.</em></strong>
               <p/>
               <img src="/monster/{2}"/>
-              '''.format(name, hit_me(name), name_hash)
+              <p/>
+              <strong><em>Also, please meet my random friend, {3}, who wants to talk to you!</em></strong>
+              <p/>
+              <img src="/monster/{3}?no_cache=1"/>
+              '''.format(name, hits, name_hash, friend_hash[:6])
     footer = '</body></html>'
 
     return header + body + footer
@@ -44,13 +51,18 @@ def mainpage():
 @app.route('/monster/<name>')
 def get_identicon(name):
 
-    name = html.escape(name, quote=True)
-    image = cache.get(name)
-    if image is None:
-        print ("Cache miss", flush=True)
+    if request.args.get('no_cache') is None:
+        name = html.escape(name, quote=True)
+        image = cache.get(name)
+        if image is None:
+            print ("Cache miss", flush=True)
+            r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+            image = r.content
+            cache.set(name, image)
+    else:
         r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
         image = r.content
-        cache.set(name, image)
+
 
     return Response(image, mimetype='image/png')
 
