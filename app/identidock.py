a3f288316521fa3bd173c926a63f6b35dc581cb7
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-04-20 12:45:03 +0100

    Use compose service names, use port 9090 in dev mode

--- app/identidock.py
+++ app/identidock.py
@@ -52,15 +52,15 @@ def get_monster(name):
         image = cache.get(name)
         if image is None:
             print('Cache miss', flush=True)
-            r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+            r = requests.get('http://monsterz-den:8080/monster/' + name + '?size=80')
             image = r.content
             cache.set(name, image)
     else:
-        r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+        r = requests.get('http://monsterz-den:8080/monster/' + name + '?size=80')
         image = r.content
 
 
     return Response(image, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=9090)
